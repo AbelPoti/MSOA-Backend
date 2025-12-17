d7b37210419ba52181fca1d3fc7484b3e7e50d48
norbinorbert <norbi_boda@yahoo.com>
2025-12-17 21:31:09 +0200

    basic Spring application template

---
+++ src/main/java/edu/bbte/replate/RePlateApplication.java
@@ -0,0 +1,13 @@
+package edu.bbte.replate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RePlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bbte/replate/RePlateApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bbte.replate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
