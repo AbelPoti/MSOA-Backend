fc4ef0c7591af80f081b8b328e9a502e00ea0794
abelke2 <potiabelke@gmail.com>
2026-06-20 11:44:33 +0300

    Introduce a new empty Spring Boot application for Auth service, set explicit ports

---
+++ Auth/src/test/java/edu/bbte/replate/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bbte.replate.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
